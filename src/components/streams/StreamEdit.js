941acfe497069d36af216926bb3eae300ded07d9
enzoesper <enzoesper@hotmail.com>
2019-06-22 22:23:01 -0300

    setting initial values to redux form to reload the edit stream page with the title and the description of the stream that is goone be edited

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchStream } from '../../actions';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
 class StreamEdit extends React.Component {
 
@@ -8,18 +9,29 @@ class StreamEdit extends React.Component {
     this.props.fetchStream(this.props.match.params.id);
   }
 
+  onSubmit = (formValues) => {
+    console.log(formValues);
+  }
+
   render() {
     if (!this.props.stream) {
       return <div> Loading...</div>
     }
 
-    return <div>{this.props.stream.title}</div>;
+    return(
+      <div>
+        <h3>Edit Stream</h3>
+        <StreamForm 
+        onSubmit={this.onSubmit} 
+        initialValues={this.props.stream} 
+        />
+      </div>
+    );
   }
 };
 
 const mapStateToProps = (state, ownProps) => {
-  
   return { stream: state.streams[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit);
