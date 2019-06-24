02fab7f51ae2865616587c7d67d002f5e0bfaaa5
enzoesper <enzoesper@hotmail.com>
2019-06-23 22:48:08 -0300

    replacing the put method with patch methd, that only modify some properties of a stream besides replacing all the stream

--- src/actions/index.js
+++ src/actions/index.js
@@ -38,9 +38,11 @@ export const fetchStream = (id) => async (dispatch) => {
 };
 
 export const editStream = (id, formValues) => async (dispatch) => {
-  const response = await streams.put(`/streams/${id}`, formValues);
+  const response = await streams.patch(`/streams/${id}`, formValues);
 
   dispatch({ type: EDIT_STREAM, payload: response.data });
+  // Do some programmatic navigation to get the user to the principal page
+  history.push('/');
 };
 
 export const deleteStream = (id) => async (dispatch) => {
